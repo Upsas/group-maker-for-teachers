9be3be72f8e52212e7159a5ead0d0b3ad8bbf0d8
Upsas <deivydas.brusokas@gmail.com>
2022-05-21 17:29:38 +0300

    fixes for ci/cd, pre-commit and docker files

--- .github/workflows/phpcs-github-actions.sh
+++ .github/workflows/phpcs-github-actions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-PHPCS=/vendor/bin/phpcs
-git log --oneline
+PHPCS=./application/vendor/bin/phpcs
+
 commitId=$(git log -n1 --format="%h")
 STAGED_FILES=$(git diff-tree --no-commit-id --name-only -r $commitId | grep \.php$)
-echo "$STAGED_FILES"
+
 if [[ "$STAGED_FILES" = "" ]]; then
 	echo "No .php files was found"
 	exit 0
@@ -12,20 +12,19 @@ fi
 echo -e "\nValidating PHPCS:\n"
 
 # Check for phpcs
-which $PHPCS &> /dev/null
-if [[ "$?" == 1 ]]; then
-  echo -e "\033[41m;30mPlease install PHPCS\033[0m"
+if [[ ! -f $PHPCS ]]; then
+  echo -e "\033[41m PHPCS was not found\033[0m"
   exit 1
 fi
 
 for FILE in $STAGED_FILES
 do
-  /vendor/bin/phpcs --standard=PSR12 "$FILE"
+  ./application/vendor/bin/phpcs --standard=PSR12 -n --ignore=/tests/* "$FILE"
   if [[ "$?" == 0 ]]; then
     echo -e "\033[42;30mPHPCS Passed: $FILE\033[0m"
   else
     echo -e "\033[41mPHPCS Failed: $FILE\033[0m"
-    PASS == 'FAILED'
+    PASS="FAILED"
   fi
 done
 
--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,26 @@
 FROM php:8-fpm
+
+## MySQL PDO Bcmath support
 RUN docker-php-ext-install pdo pdo_mysql bcmath
+
+## Update package information + git and curl
+RUN apt-get update && apt-get install -y \
+    git \
+    curl
+
+## Install Composer
+RUN curl -sS https://getcomposer.org/installer \
+  | php -- --install-dir=/usr/local/bin --filename=composer
+
+## Install zip libraries and extension
+RUN apt-get install --yes git zlib1g-dev libzip-dev \
+    && docker-php-ext-install zip
+
+## Install node
+RUN curl -sL https://deb.nodesource.com/setup_16.x | bash -
+RUN apt-get install -y nodejs
+
+## Install xdebug
+RUN pecl install xdebug \
+    && docker-php-ext-enable xdebug
+
