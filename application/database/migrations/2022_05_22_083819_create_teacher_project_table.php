e1e9fc2d297d94aab391bb14d2b31eed0f696f1b
Upsas <deivydas.brusokas@gmail.com>
2022-05-22 16:28:11 +0300

    added migrations + updated user migration

---
+++ application/database/migrations/2022_05_22_083819_create_teacher_project_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_teacher', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('teacher_id')->constrained();
+            $table->foreignId('project_id')->constrained();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teacher_project');
+    }
+};
