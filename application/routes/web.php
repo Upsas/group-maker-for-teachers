40f5691d2513446ee31639c56c2eac349df53232
Upsas <deivydas.brusokas@gmail.com>
2022-05-23 20:15:36 +0300

    updated styles for generated files + small routes changes

--- application/routes/web.php
+++ application/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
+Route::get('/', [AuthenticatedSessionController::class, 'create'])->name('login');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
 require __DIR__ . '/auth.php';
