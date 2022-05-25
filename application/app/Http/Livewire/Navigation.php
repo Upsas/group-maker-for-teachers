3f212bd3da3e782e7b3e33625a0aa9f2ca729316
Upsas <deivydas.brusokas@gmail.com>
2022-05-25 10:46:06 +0300

    added navigation and project info table livewire + blade components

--- application/app/Http/Livewire/Navigation.php
+++ application/app/Http/Livewire/Navigation.php
@@ -3,12 +3,15 @@
 namespace App\Http\Livewire;
 
 use App\Repositories\TeacherRepository;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Navigation extends Component
 {
-    public function render(TeacherRepository $teacherRepo)
+    public function render(TeacherRepository $teacherRepo): Factory|View|Application
     {
         return view('livewire.navigation', ['teacher' => $teacherRepo->getDataByWhere('user_id', Auth::id())->first()]);
     }
---
+++ application/app/Http/Livewire/ProjectInfoTable.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Project;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Livewire\Component;
+
+class ProjectInfoTable extends Component
+{
+    public Project $project;
+
+    public function mount(Project $project): void
+    {
+        $this->project = $project;
+    }
+
+    public function render(): Factory|View|Application
+    {
+        return view('livewire.project-info-table');
+    }
+}
