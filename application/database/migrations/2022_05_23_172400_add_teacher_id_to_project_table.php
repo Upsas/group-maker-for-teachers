2b9130606ce8bc620da2857bfc4b16326bd0b95c
Upsas <deivydas.brusokas@gmail.com>
2022-05-25 10:30:18 +0300

    updated teacher migrations + seeder + model

---
+++ application/database/migrations/2022_05_23_172400_add_teacher_id_to_project_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->unsignedBigInteger('teacher_id')->after('students_per_group');
+            $table->foreign('teacher_id')->references('id')->on('teachers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn('teacher_id');
+        });
+    }
+};
--- application/database/seeders/TeacherSeeder.php
+++ application/database/seeders/TeacherSeeder.php
@@ -2,11 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Project;
-use App\Models\Teacher;
-use App\Models\User;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -25,15 +21,6 @@ class TeacherSeeder extends Seeder
                 'created_at' => now(),
                 'updated_at' => now()
             ]);
-        }
-        $project = Project::inRandomOrder()->get()->all();
-        foreach (Teacher::all() as $teacher) {
-            $randomNumber = rand(0, 2);
-            for ($i = 0; $i < $randomNumber; $i++) {
-                $teacher->projects()->attach($project[$i]);
-                unset($project[$i]);
-                $project = array_values($project);
-            }
         }
     }
 }
