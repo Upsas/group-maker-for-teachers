20cd3b4c51dcc5ddf2aee9da314af60519f39564
Upsas <deivydas.brusokas@gmail.com>
2022-05-25 10:31:09 +0300

    updated student migrations + seeder + model

--- application/app/Models/Student.php
+++ application/app/Models/Student.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
@@ -12,4 +13,9 @@ class Student extends Model
     use SoftDeletes;
 
     protected $guarded = [];
+
+    public function groups(): BelongsToMany
+    {
+        return $this->belongsToMany(Group::class);
+    }
 }
