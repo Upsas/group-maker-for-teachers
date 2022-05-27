634b591dc6f3d79810b5a53168b916889bdb352d
Deivydas Brusokas <61560970+Upsas@users.noreply.github.com>
2022-05-27 20:40:36 +0300

    added assign group logic (#9)

--- application/app/Http/Livewire/GroupsTable.php
+++ application/app/Http/Livewire/GroupsTable.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Project;
+use App\Repositories\StudentRepository;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -10,22 +11,34 @@ use Livewire\Component;
 
 class GroupsTable extends Component
 {
-    protected $listeners = ['renderGroupTables'];
+    protected $listeners = ['renderGroupTables' => '$refresh'];
 
     public Project $project;
+    public $selectedStudent = '';
 
     public function mount(Project $project): void
     {
         $this->project = $project;
     }
 
-    public function renderGroupTables(): void
+    public function selected(int $currentGroupId, StudentRepository $studentRepo): void
     {
-        $this->render();
+        $student = $studentRepo->where('full_name', $this->selectedStudent)->first();
+        $studentGroups = $student->groups();
+
+        if ($studentGroups->get()->isEmpty()) {
+            $studentGroups->attach($currentGroupId);
+        } else {
+            $studentGroups->sync($currentGroupId);
+        }
+        $this->reset('selectedStudent');
+        $this->emit('renderStudentTable');
     }
 
     public function render(): Factory|View|Application
     {
-        return view('livewire.groups-table');
+        $groups = $this->project->groups()->get();
+        $allStudents = $this->project->students();
+        return view('livewire.groups-table', ['groups' => $groups, 'allStudents' => $allStudents]);
     }
 }
--- application/resources/views/livewire/groups-table.blade.php
+++ application/resources/views/livewire/groups-table.blade.php
@@ -1,52 +1,61 @@
-<div class="grid grid-rows-1 sm:grid-cols-2 lg:w-5/12">
-    @for ($a = 0; $a < $project->groups; $a++)
-    @php
-        $students = $project->groups()->first()?->students()->get()->toArray() ?? [];
-        $groups = $project->groups()->get()->toArray()
-    @endphp
-    <div class="w-full p-6 font-mono">
-        <div class=" w-full mb-2 overflow-hidden rounded-lg shadow-lg">
-            <div class="overflow-x-auto">
-                <table class="w-full">
-                    <thead>
-                    <tr class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-300 uppercase
+<div class="grid grid-rows-1 sm:grid-cols-2 lg:w-5/12" id="groups">
+    @forelse ($groups as $group)
+        <div class="w-full p-6 font-mono">
+            <div class=" w-full mb-2 overflow-hidden rounded-lg shadow-lg">
+                <div class="overflow-x-auto">
+                    <table class="w-full">
+                        <thead>
+                        <tr class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-300 uppercase
                     border-b border-gray-300">
-                        <th class="px-4 py-3 text-center sm:w-2/5">{{"Group #" }}{{array_key_exists($a, $groups) ?
-                        $groups[$a]['group_index'] : ''}}</th>
-                    </tr>
-                    </thead>
-                    <tbody class="bg-white">
-
-                    @for ($i = 0; $i < $project->students_per_group; $i++)
-                        @if(array_key_exists($i, $students))
-                        <tr class="text-gray-700">
-                        <td class="px-4 py-3 border border-gray-300">
-                            <div class="h-10 flex justify-center items-center">
-                                <p class="font-semibold text-black text-center">{{$students[$i]['full_name']}}</p>
-                            </div>
-                        </td>
-                    </tr>
-                    @else
-                    <tr class="text-gray-700">
-                        <td class="px-4 py-3 h-10 border border-gray-300">
-                            <div class=" h-10 text-sm">
-                                <select class="text-sm sm:text-base text-center w-full" name="student"
-                                        id="student">
-                                    <option value="none" selected disabled hidden>Assign student</option>
-                                    @forelse($project->students()->get() as $student)
-                                    <option value={{$student->full_name}}>{{ $student->full_name }}</option>
-                                        @empty
-                                    @endforelse
-                                </select>
-                            </div>
-                        </td>
-                    </tr>
-                    @endif
-                    @endfor
-                    </tbody>
-                </table>
+                            <th class="px-4 py-3 text-center sm:w-2/5">{{"Group #" }}{{$group->group_index}}</th>
+                        </tr>
+                        </thead>
+                        <tbody class="bg-white">
+                        @php
+                            $students = $group->students()->get()->toArray();
+                            $studentsIds = [];
+                        @endphp
+                        @for ($i = 0; $i < $project->students_per_group; $i++)
+                            @if(array_key_exists($i, $students))
+                                @php
+                                    $studentsIds[] = $students[$i]['id'];
+                                @endphp
+                                <tr class="text-gray-700">
+                                    <td class="px-4 py-3 border border-gray-300">
+                                        <div class="h-10 flex justify-center items-center">
+                                            <p class="font-semibold text-black text-center">{{$students[$i]['full_name']}}</p>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @else
+                                <tr  class="text-gray-700">
+                                    <td class="px-4 py-3 h-10 border border-gray-300">
+                                        <div class="h-10 text-sm">
+                                            <select
+                                                wire:model="selectedStudent"
+                                                wire:change="selected({{$group->id}})"
+                                                class="text-sm sm:text-base text-center w-full"
+                                                name="student">
+                                                <option value="" selected disabled hidden>Assign student</option>
+                                                @forelse($project->students()->whereNotIn('students.id',$studentsIds)
+                                                ->get() as $student)
+                                                    <option value="{{$student->full_name}}">
+                                                        {{ $student->full_name}}</option>
+                                                @empty
+                                                @endforelse
+                                            </select>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endif
+                        @endfor
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-@endfor
+    @empty
+        {{__('Something went wrong')}}
+    @endforelse
+
 </div>
